d9e9b1b87c041c1981400830934b99b14ac54b0d
NoGoBo616 <137913897+NoGoBo616@users.noreply.github.com>
2025-11-06 20:21:05 +0100

    muchas cosas

--- FPS_Horror_Miedo_Metro_Proyect/Assets/_FPS_Root/Scripts/Managers/SceneLoader.cs
+++ FPS_Horror_Miedo_Metro_Proyect/Assets/_FPS_Root/Scripts/Managers/SceneLoader.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,17 +6,41 @@ public class SceneLoader : MonoBehaviour
 {
     [Header("Scene Management")]
     public int sceneToLoad;
+    public GameObject aviso;
+
+    public void Cambio()
+    {
+        LoadScene(sceneToLoad);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            LoadScene(sceneToLoad);
+            Cambio();
         }
     }
 
     public void LoadScene(int sceneToLoad)
     {
         SceneManager.LoadScene(sceneToLoad);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Aviso()
+    {
+        StartCoroutine(Avisar());
+    }
+
+    IEnumerator Avisar()
+    {
+        aviso.SetActive(true);
+        yield return new WaitForSeconds(20);
+        aviso.SetActive(false);
+        yield return null;
     }
 }
